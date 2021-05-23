95ef1aeca9c6c60b0ea41df595bb0473ac4daf54
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-23 22:16:10 +0900

    2-06 답변기능
    답변기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -12,5 +13,11 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
 
 # Create your views here.
