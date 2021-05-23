995ab6304875cb640fe02434ff504b9485277c4f
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-23 16:34:36 +0900

    2-04 질문목록
    질문목록과 질문 상세기능 구현완료

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요, 환영합니다!!")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
 
 # Create your views here.
