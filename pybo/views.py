f86589a0dd7f7223ea997c5d92ddd83da5e2dbb1
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-24 22:46:01 +0900

    질문등록
    질문 등록기능 만드는 중

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -19,5 +20,18 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    form = QuestionForm(request.POST)
+    if form.is_valid():
+        question = form.save(commit=False)
+        question.create_date = timezone.now()
+        question.save()
+        return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
 
 # Create your views here.
