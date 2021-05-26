0067296198c00629f73d3429181f8bc90c41697d
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-26 16:32:38 +0900

    3-05 login, logout
    로그인 로그아웃 구현
    부분부분 이해안가는 부분 있긴함

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
