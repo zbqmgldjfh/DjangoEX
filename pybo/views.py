2f93ae1fafebd54145e775b340f740735f24e98c
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-26 15:16:26 +0900

    3-03 pagination
    페이징 기능 구현

--- pybo/views.py
+++ pybo/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
