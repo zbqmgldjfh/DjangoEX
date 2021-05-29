e904a253d49a6e7988845eb6882ffc1c161600c7
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-05-29 14:51:49 +0900

    3-11 views.py 분리

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index')
+    path('', base_views.index, name='index'),
 ]
